d68d51fff16feb660b030cf4c99baa94f37fb9df
nathannorris <nlnorris@arkbluecross.com>
2024-05-08 11:18:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
-session = get_active_session()
+#session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
